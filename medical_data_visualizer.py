842f929d6ab696ade6b52c01861e923954532c54
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-09-05 11:26:14 -0300

    comments #2

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -8,15 +8,17 @@ df = pd.read_csv('medical_examination.csv')
 
 # 2
 
-overweight_list = []
-bmi = df['weight']/((df['height']/100)**2)
-for i in bmi:
-    if i > 25:
-        overweight_list.append(1) #overweight
-    else:
-        overweight_list.append(0) #not overweight
+overweight_list = [] # Create an empty List that will receive all the values related to overweight
 
-df['overweight'] = overweight_list
+bmi = df['weight']/((df['height']/100)**2) # calculus of ibm and adjustment of unit of measure for height
+
+for i in bmi: # Since ibm returns a list with index and value, now I do an iteration to classify each bmi in 0 or 1 based on the criteria defined in the question and add this information in the overweight_list 
+    if i > 25:
+        overweight_list.append(1) #1 for overweight
+    else:
+        overweight_list.append(0) #0 for not overweight
+
+df['overweight'] = overweight_list # finally, I add a new column by: declaring a new column name with the binary list: overweight_list
 
 # 3
 
--- notes.md
+++ notes.md
@@ -1,3 +1,7 @@
-### Referências
+## Explicação
 
+## Referências
+
+### Part 2
+**How to create collumns with pandas**\
 https://www.youtube.com/watch?v=IKiDSOUTQX8 
